6117cd60ba4a61bbaadab6168b26a654ac50343f
Veera Sankar <sankarsivam6991@gmail.com>
2024-10-26 22:28:29 +0530

    OTT Updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MovieList from "./Components/MovieList";
-import MovieDetails from "./MovieDetails";
+import MovieDetails from "./Components/MovieDetails";
 
 function App() {
   return (
